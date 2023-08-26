34e64ae061ddf1cac33d2095847b0f161e2ad3de
valerine <valerine.chemutai@student.moringaschool.com>
2023-08-26 03:22:36 +0300

    tookMeTwoDays

---
+++ src/components/ArticleList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({posts}) {
+  console.log(posts);
+  const articles = posts.map((post) =>(
+    <Article
+    key={post.id}
+    title={post.title}
+    date={post.date}
+    preview={post.preview}
+    minutes= {post.minutes}
+    />
+  ));
+  return (
+    <main>
+      {articles}
+    </main>
+  )
+}
+
+export default ArticleList;
